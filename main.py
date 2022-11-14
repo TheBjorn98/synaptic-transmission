2dada29ba95f43a29287be4d99d8f72845be9128
Erlend <80318998+erlendlokna@users.noreply.github.com>
2022-11-14 18:58:04 +0000

    Update main.py
    Made a main file for the simulation.

--- main.py
+++ main.py
@@ -1,67 +1,29 @@
 from numerics import *
+from reaction import *
 import numpy as np
 import matplotlib.pyplot as plt
 
 import time
 
 def main():
-
-    N_iter = 10
-    R = 10
-    t = 0
-
-    #10 x 10 x 10
-    grid_vec = np.random.uniform(0, 1, 10*10*10) #flatten solution from difusion. 
-    reaction_state = np.array([100.30, 3.2, 1.1]) #[N, R, C] initial
-
-    state_hist = [reaction_state]
-    t_hist = [t]
-
-    t0 = time.time()
+    #simulation parameters:
+     N_its = 1000
+    
+    #initial conditions
+    reaction_state = np.array([0, 0, 0])
+    grid_vec = np.zeros(1000)
+    t0 = 0
 
     
-    for _ in range(10000):
+    #run iterations
+    for _ in range(N_its):
 
-        new_t, new_s = update_reaction_state(grid_vec, t, reaction_state, 0.01)
+        #grid_vec = update_diffusion() 
         
-        t += 0.1
-        state_hist.append(new_s)
-        t_hist.append(new_t)
-
-    t1 = time.time()
-
-    print(f"time: {t1-t0}")
-
-    state_hist = np.array(state_hist)
-    N = np.array([s[0] for s in state_hist])
-    R = np.array([s[1] for s in state_hist])
-    C = np.array([s[2] for s in state_hist])
-
-    plt.plot(t_hist, N)
-    plt.plot(t_hist, R)
-    plt.plot(t_hist, C)
-    plt.show()
-    print(N)
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        #updating state:
+        t, reaction_state = update_reaction_state(t, reaction_state, grid_vec, 0.01)
+        
+  
 
 
 if __name__ == "__main__":
