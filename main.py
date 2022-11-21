7871d9c69d65eda9ce4c631a3c61009da67913f4
Bjørnar Kaarevik <bjrnarkaarevik@gmail.com>
2022-11-21 19:24:26 +0100

    moved plotting out of main file

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import time
 
-from reaction import update_reaction, make_reaction_ode
-from grid_3d_cartesian import update_diffusion, iterate_system_bw_euler
+from reaction import make_reaction_ode
+from grid_3d_cartesian import iterate_system_bw_euler
 
 
 if __name__ == "__main__":
@@ -19,22 +19,26 @@ if __name__ == "__main__":
     N0 = 5000
     rho_R0 = 1000 / 1e-12
     R0 = A_terminal * rho_R0
-    ax, ay, az = D * dx**2 / Lx**2, D * dy**2 / Ly**2, D * dz**2 / Lz**2
+    ax, ay, az = D * dx / Lx, D * dy / Ly, D * dz / Lz
+    # ax, ay, az = D * dx**2 / Lx**2, D * dy**2 / Ly**2, D * dz**2 / Lz**2
     ax, ay, az = ax/az, ay/az, az/az
     alphas = (ax, ay, az)
 
     ### Time parameters for the simulation
     N_time = 3000
     T = Lz / D
-    Tf = 100*T
+    # Tf = 100*T
+    Tf = 1
+    # Tf = 0.5
     dt = Tf / N_time
-    N_time = 2000
+    # ax, ay, az = dx**2 * T / Lx**2, dy**2 * T / Ly**2, dz**2 * T / Lz**2
     
     ### Setup for the reaction
     k_on = 4e6
     k_off = 5
-    pi_1 = k_on * T * R0
-    pi_2 = k_off * R0 * T / N0
+    pi_1 = k_on * T * R0 
+    # pi_2 = k_off * T * R0 / N0  # Dividing by N0
+    pi_2 = k_off * T * R0  # Not dividing by N0
     reaction = make_reaction_ode(pi_1, pi_2)
     # reaction = make_reaction_ode(1, 1)
 
@@ -66,40 +70,42 @@ if __name__ == "__main__":
     gvs, rs, cs = iterate_system_bw_euler(N_space, N_time, dt, (n_init, r_init, c_init), alphas, reaction)
 
 
-    fig = plt.figure()
-    axs = [fig.add_subplot(1, 9, i+1) for i in range(9)]
-    # plot_loc = np.arange(1, N_space, N_space//8)
-    plot_loc = [1 + i*N_space//8 for i in range(8)]
-    plot_loc.append(N_space-2)
-    print(plot_loc)
+    ### Normal to z-axis plotting
+    # fig = plt.figure()
+    # axs = [fig.add_subplot(1, 9, i+1) for i in range(9)]
+    # # plot_loc = np.arange(1, N_space, N_space//8)
+    # plot_loc = [1 + i*N_space//8 for i in range(8)]
+    # plot_loc.append(N_space-2)
+    # print(plot_loc)
+    # def animate(i):
+    #     g = gvs[i, :].reshape((N_space, N_space, N_space))
+    #     for j, ax in enumerate(axs):
+    #         ax.clear()
+    #         ax.imshow(g[:, :, plot_loc[j]], vmin=0, vmax=R0/N0)
+    #     axs[0].set_title(f"t = {i * dt:.4f}")
+    #     return axs
+    # anim = FuncAnimation(fig, animate, frames=N_time, blit=True, interval=10)
+    # plt.show()
 
-    def animate(i):
-        g = gvs[i, :].reshape((N_space, N_space, N_space))
+    ### Along z-axis plotting
+    # fig = plt.figure()
+    # ax = fig.add_subplot(1, 1, 1, projection="3d")
+    # rng = np.linspace(0, 1, num=N_space)
+    # xx, zz = np.meshgrid(rng, rng)
+    # def animate(i):
+    #     g = gvs[i, :].reshape((N_space, N_space, N_space))
+    #     ax.clear()
+    #     # ax.imshow(g[:, N_space//2, :], vmin=0, vmax=R0/N0, cmap="Purples")
+    #     ax.plot_surface(xx, zz, g[:, N_space//2, :], cmap="Reds")
+    #     # ax.set_title(f"t = {i}")
+    #     return [ax]
+    # anim = FuncAnimation(fig, animate, frames=N_time, blit=True, interval=1)
+    # plt.show()
+    # # plt.savefig("conc_surface_mid.gif")
 
-        for j, ax in enumerate(axs):
-            ax.clear()
-            ax.imshow(g[:, :, plot_loc[j]], vmin=0, vmax=R0/N0)
-            
-            
-        axs[0].set_title(f"t = {i * dt:.4f}")
-        return axs
-
-    anim = FuncAnimation(fig, animate, frames=N_time, blit=True, interval=10)
-
-    plt.show()
-
-    masses = [np.sum(gvs[i, :]) for i in range(N_time)]
-    plt.plot(masses)
-    plt.axhline(1, linestyle="--", color="k", linewidth=1)
-    plt.show()
-
-    rvals = np.array([np.sum(rs[i, :]) for i in range(N_time)])
-    cvals = np.array([np.sum(cs[i, :]) for i in range(N_time)])
-    nvals = np.array([np.sum(gvs[i, N_space**3 - N_space**2:]) for i in range(N_time)])
-
-    plt.plot(rvals)
-    plt.plot(cvals)
-    plt.plot(nvals)
-    plt.plot(rvals + cvals)
-    plt.show()
+    ### Change in mass of N, some it picked up by R, some leaks out
+    # masses = [np.sum(gvs[i, :]) for i in range(N_time)]
+    # plt.plot(masses)
+    # plt.axhline(1, linestyle="--", color="k", linewidth=1)
+    # plt.show()
 
